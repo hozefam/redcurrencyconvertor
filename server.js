3121a61c423bc90deec5e28f8b8dd17da65a0a02
Hozefa Mangalorewala <hozefam@gmail.com>
2018-11-10 11:49:30 +0300

    UI done

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const redis = require('redis');
 const app = express();
 
-const API_URL = 'http://api.fixer.io';
+const API_URL = 'http://data.fixer.io/api';
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 app.get('/rate/:date', (req, res) => {
   const { date } = req.params;
-  const url = `${API_URL}/${date}?base=USD`;
+  const url = `${API_URL}/${date}?access_key=3c9aaafbf633ba5df9d4900ee996b183`;
 
   axios
     .get(url)
@@ -26,7 +26,7 @@ app.get('/rate/:date', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
