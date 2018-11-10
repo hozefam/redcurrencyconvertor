62e1aeb363d406bcb466946adf9e6f9ab735a48b
Hozefa Mangalorewala <hozefam@gmail.com>
2018-11-10 12:30:37 +0300

    Added Redis Caching

--- Readme.md
+++ Readme.md
@@ -6,6 +6,8 @@ Simple currency convertor app build using express, redis, axios and bulma css.
 
 To learn Redis caching
 
+![Preview](https://user-images.githubusercontent.com/6629490/48299845-2b9a2980-e4e4-11e8-9792-40678cd27b57.png)
+
 ## Tech/framework used :computer:
 
 <b>Built with</b>
--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app = express();
 
 const API_URL = 'http://data.fixer.io/api';
 
+const client = redis.createClient();
+client.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+client.on('error', err => {
+  console.log(`Error : ${err}`);
+});
+
 app.get('/', (req, res) => {
   res.sendFile('index.html', {
     root: path.join(__dirname, 'views')
@@ -16,14 +25,32 @@ app.get('/rate/:date', (req, res) => {
   const { date } = req.params;
   const url = `${API_URL}/${date}?access_key=3c9aaafbf633ba5df9d4900ee996b183`;
 
-  axios
-    .get(url)
-    .then(response => {
-      return res.json({ rates: response.data.rates });
-    })
-    .catch(error => {
-      console.log(error);
+  const countKey = `EUR:${date}:count`;
+  const ratesKey = `EUR:${date}:rates`;
+
+  client.incr(countKey, (err, count) => {
+    client.hgetall(ratesKey, (err, rates) => {
+      if (rates) {
+        console.log('Cached');
+        return res.status(200).json({ rates, count });
+      }
+
+      axios
+        .get(url)
+        .then(response => {
+          client.hmset(ratesKey, response.data.rates, (err, result) => {
+            if (err) {
+              console.log(err);
+            }
+          });
+
+          return res.status(200).json({ count, rates: response.data.rates });
+        })
+        .catch(error => {
+          console.log(res.status(400).json(error.response.data));
+        });
     });
+  });
 });
 
 const port = process.env.PORT || 5000;
